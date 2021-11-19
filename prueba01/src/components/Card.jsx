5761d540d4402787dd3fea119b1323c4a7e5b12e
miguelCidPaz <miguel.cid.paz@gmail.com>
2021-11-19 22:58:31 +0100

    card terminada

--- prueba01/src/components/Card.jsx
+++ prueba01/src/components/Card.jsx
@@ -2,13 +2,24 @@ import React, { Component } from "react";
 import '../CSS/style.css';
 
 class Card extends Component {
+    render() {
+        return (
+            <div className="body-card">
+                <HeadCard content={this.props.presentation} />
+                <BodyCard content={this.props.dates} />
+            </div>
+        )
+    }
+}
+
+class HeadCard extends Component {
     constructor(props) {
         super(props);
         this.state = { ...this.props.content };
     }
     render() {
         return (
-            <div className="body-card">
+            <div className="body-head-card">
                 <div className="frame-picture">
                     <img src={this.state.photo} alt="#" />
                 </div>
@@ -17,6 +28,28 @@ class Card extends Component {
                 <p className="text-card text-card-link">{this.state.link}</p>
             </div>
         )
+    }
+}
+
+class BodyCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { ...this.props.content }
+    }
+    render() {
+        return <div className="body-date-card">
+            <div className="date devices">
+                <div className="ornament"></div><p className="device-text">Devices Used - {this.state.devices}</p>
+            </div>
+            <div className="date location">
+                <div className="ornament"></div><p className="location-text">Location - {this.state.location}</p>
+            </div>
+            <div className="date profile">
+                <div className="ornament"></div><p className="profile-text">{this.state.profile} - Profile</p>
+            </div>
+
+            <p className="total-amount">Total Amount Spent $ {this.state.amount}</p>
+        </div>
     }
 }
 
